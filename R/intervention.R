cf9f8dea7d943cac6a64d37d437737c33ee34419
Robert Badgett <rbadgett@kumc.edu>
2023-03-22 23:01:45 -0500

    tried to p-value for I2

--- R/intervention.R
+++ R/intervention.R
@@ -231,7 +231,7 @@ if (type=="ignore") # Meta-analysis without subgroup
 			fixed = FALSE, common = FALSE, random = TRUE, 
 			# subgroup = TRUE, print.Q.subgroup = FALSE, # not relevant as no subgroups here
 			# resid.hetstat = TRUE,  # not relevant as no subgroups here
-			print.I2.ci = TRUE, print.tau2 = FALSE, print.pval.Q = FALSE,
+			print.I2.ci = TRUE, print.tau2 = FALSE, # print.pval.Q = FALSE,
 			label.left=lefthand, label.right=righthand,text.random=analyticmethod, fs.random=12, ff.random = 1, ff.hetstat=2, fs.hetstat=12)
 	#grid.text(topic, 0.5, 0.97, gp = gpar(fontsize = 14, fontface = "bold"))
 	grid.text(topic, 0.5, 0.97, gp = gpar(fontsize = 14, fontface = "bold"))
@@ -284,7 +284,7 @@ if (grepl("subgroup",type))
 			fixed = FALSE, common = FALSE, random = TRUE, 
 			subgroup = TRUE, print.Q.subgroup = FALSE, 
 			#resid.hetstat = TRUE, 
-			print.I2.ci = TRUE, print.tau2 = FALSE, print.pval.Q = FALSE,
+			print.I2.ci = TRUE, print.tau2 = FALSE, # print.pval.Q = FALSE,
 			label.left=lefthand, label.right=righthand,text.random=analyticmethod, fs.random=12, ff.random = 1, ff.hetstat=2, fs.hetstat=12)
 	#stop(paste("stop line 280 with: ", topic, pubbiastext, sep=", "))
 	grid.text(topic, 0.5, 0.97, gp = gpar(fontsize = 14, fontface = "bold"))
