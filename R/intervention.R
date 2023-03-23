27811c45f4beab5efd32c5eacdde5017e7f7bbd1
Robert Badgett <rbadgett@kumc.edu>
2023-03-22 23:13:51 -0500

    Update intervention.R

--- R/intervention.R
+++ R/intervention.R
@@ -229,9 +229,8 @@ if (type=="ignore") # Meta-analysis without subgroup
 	#stop(paste(topic,lefthand, righthand, sep=", "))
 	forest(meta1, sortvalue, xlim=xlimits, col.diamond="blue", col.diamond.lines="blue", title = topic, 
 			fixed = FALSE, common = FALSE, random = TRUE, 
-			# subgroup = TRUE, print.Q.subgroup = FALSE, # not relevant as no subgroups here
-			# resid.hetstat = TRUE,  # not relevant as no subgroups here
-			print.I2.ci = TRUE, print.tau2 = FALSE, # print.pval.Q = FALSE,
+			#resid.hetstat = TRUE, 
+			print.I2.ci = TRUE, print.tau2 = FALSE, print.pval.Q = FALSE,
 			label.left=lefthand, label.right=righthand,text.random=analyticmethod, fs.random=12, ff.random = 1, ff.hetstat=2, fs.hetstat=12)
 	#grid.text(topic, 0.5, 0.97, gp = gpar(fontsize = 14, fontface = "bold"))
 	grid.text(topic, 0.5, 0.97, gp = gpar(fontsize = 14, fontface = "bold"))
@@ -282,9 +281,9 @@ if (grepl("subgroup",type))
 	#stop(paste("stop line 277 with: ", sortvalue, lefthand, righthand, analyticmethod, sep=", "))
 	forest(meta1, sortvalue, col.diamond="blue", col.diamond.lines="blue", 
 			fixed = FALSE, common = FALSE, random = TRUE, 
-			subgroup = TRUE, print.Q.subgroup = FALSE, 
+			subgroup = TRUE, 
 			#resid.hetstat = TRUE, 
-			print.I2.ci = TRUE, print.tau2 = FALSE, # print.pval.Q = FALSE,
+			print.I2.ci = TRUE, print.tau2 = FALSE, print.pval.Q = FALSE,
 			label.left=lefthand, label.right=righthand,text.random=analyticmethod, fs.random=12, ff.random = 1, ff.hetstat=2, fs.hetstat=12)
 	#stop(paste("stop line 280 with: ", topic, pubbiastext, sep=", "))
 	grid.text(topic, 0.5, 0.97, gp = gpar(fontsize = 14, fontface = "bold"))
