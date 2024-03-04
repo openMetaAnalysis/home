ca166924363f89eb7597aebfcd7f7fc1f9fe2fbc
Robert Badgett <rbadgett@kumc.edu>
2024-03-04 11:22:44 -0600

    Update intervention.R

--- R/intervention.R
+++ R/intervention.R
@@ -227,7 +227,7 @@ if (type=="ignore") # Meta-analysis without subgroup
 		sortvalue <- 1/meta1$w.random
 		}
 	#stop(paste(topic,lefthand, righthand, sep=", "))
-	meta::forest (meta1, sortvalue = sortvalue, xlim=xlimits, col.diamond="red", col.diamond.lines="red", title = topic, 
+	meta::forest (meta1, sortvalue = sortvalue, xlim=xlimits, col.diamond="blue", col.diamond.lines="blue", title = topic, 
 			fixed = FALSE, common = FALSE, random = TRUE, 
 			resid.hetstat = TRUE, addrows = 2,
 			print.I2.ci=TRUE, print.tau2=FALSE, print.p=FALSE, 
@@ -287,7 +287,7 @@ if (grepl("subgroup",type)) #  Meta-analysis with subgroups
 		sortvalue <- 1/meta1$w.random
 		}
 	#stop(paste("stop line 277 with: ", sortvalue, lefthand, righthand, analyticmethod, sep=", "))
-	forest(meta1, sortvalue = sortvalue, col.diamond="red", col.diamond.lines="red", 
+	forest(meta1, sortvalue = sortvalue, col.diamond="blue", col.diamond.lines="blue", 
 			fixed = FALSE, common = FALSE, random = TRUE, 
 			subgroup = TRUE, print.Q.subgroup = FALSE, print.pval.Q = TRUE, 
 			resid.hetstat = TRUE, addrows = 2,# restored 05/02/2023 2023-05-02
